821717ea2581fec7c53eba11d027fc566615541b
Alex Millerman <amillerman@users.noreply.github.com>
2022-11-18 12:16:47 -0500

    Time: 131 ms (92.32%), Space: 47.5 MB (51.19%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -2,26 +2,4 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    let charVals = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000
-    };
-    let numVals = {
-        1: 0,
-        5: 1,
-        10: 1,
-        50: 10,
-        100: 10,
-        500: 100,
-        1000: 100
-    }
-    return s.split('').map(c => charVals[c]).reduce((p, c) => 
-        p + c - ((c - 1) && (numVals[c]) * (p/(numVals[c]) % 2 * 2))
-    );
-};
+var romanToInt=function($){let _={I:1,V:5,X:10,L:50,C:100,D:500,M:1e3},n={1:0,5:1,10:1,50:10,100:10,500:100,1e3:100};return $.split("").map($=>_[$]).reduce(($,_)=>$+_-(_-1&&n[_]*($/n[_]%2*2)))};
