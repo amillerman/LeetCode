40358a52acb44b9a35fbd9923d1e28361f9d3913
Alex Millerman <amillerman@users.noreply.github.com>
2022-11-20 14:37:07 -0500

    Time: 0 ms (100.00%), Space: 5.6 MB (51.62%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.c
@@ -0,0 +1,13 @@
+bool isUgly(int n){
+    if (n < 1) return false;
+    int prime = 0;
+    do{
+        prime += prime == 2 ? 1 : 2;
+        while (n%prime == 0)
+        {
+            n /= prime;
+        }
+    }while(prime < 7 && n > 1);
+    return prime < 7;
+
+}
