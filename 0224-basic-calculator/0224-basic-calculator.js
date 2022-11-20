51ae5420a0495c56fe93a86abb0bf1943fa8b4f7
Alex Millerman <amillerman@users.noreply.github.com>
2022-11-20 17:42:21 -0500

    Time: 96 ms (86.05%), Space: 48.6 MB (33.53%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.js
+++ 0224-basic-calculator/0224-basic-calculator.js
@@ -2,7 +2,58 @@
  * @param {string} s
  * @return {number}
  */
-var calculate = function(s) {
+
+var calculate = function(s){
+  return calcInBrackets(s).total;
+};
+let calcInBrackets = (s) => {
+    let index = 0;
+    let total = 0;
+    let calc = {};
+    s = s.replace(/ /g, "");
+    // console.log("brackets", s);
+    while (s[index] != undefined){
+        // console.log(s[index]);
+        if (s[index] == '('){
+            // console.log("recurse", s.substring(index + 1 + (s[index] == '-' ? 1 : 0)));
+            ++index;
+            calc = calcInBrackets(s.substring(index));
+            total += calc.total;
+            index += calc.index + 1;
+            // console.log(" recurse calc'd", s, calc, total, index);
+        }else if ((s[index] == '-' || s[index] == '+') && s[index+1] == '('){
+                // index += 2;
+                calc = calcInBrackets(s.substring(index + 2));
+            // console.log("calc'd", s, calc, total, index);
+            if (s[index] == '-'){
+                total -= calc.total;
+                index += calc.index + 3;
+            }
+            else {
+                total += calc.total;
+                index += calc.index + 3;
+            }
+        }else
+        if (s[index] == ')' || isNaN(parseInt(s.substring(index)))){
+            // console.log("done", {total, index: index});
+            return {total, index: index};
+        }else {
+            let c =  parseInt(s.substring(index));
+            // console.log("num before", total, index, s[index], s.substring(index));
+            total += c;
+            index +=  c.toString().length + (s[index] == '+' ? 1 : 0);
+            // console.log("num after", total, index, s[index]);
+        }
+        // console.log("while agian", total, index);
+    }
+    // console.log("actually done");
+    return {total, index};
+}
+
+
+
+
+var calculateOld = function(s) {
     let bracx = /\(([+-\d]*)\)/g;
     s = s.replace(/ /g, "").replace(/\-\-/g, "+").replace(/\+\-/g, '-').replace(/\-\=/g, '-');
     do{
